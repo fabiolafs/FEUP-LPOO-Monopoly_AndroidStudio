cffe3fc1b0df2449b812d419351a1b7bf1589a52
agfac <andregfcorreia@gmail.com>
2016-06-10 21:52:06 +0100

    Buying Activity image updated

--- app/src/main/java/com/example/fabiola/monopoly/BuyingActivity.java
+++ app/src/main/java/com/example/fabiola/monopoly/BuyingActivity.java
@@ -14,15 +14,16 @@ public class BuyingActivity extends Activity {
 
     public static boolean active = false;
 
-    public static String currImage=PlayingActivity.currImage;
+    public static String currImage;
 
     Timer timer;
-    MyTimerTask myTimerTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buying);
+
+        currImage =PlayingActivity.currImage;
 
         View yesbutton = findViewById(R.id.button3);
         yesbutton.setOnClickListener(new View.OnClickListener() {
@@ -70,122 +71,98 @@ public class BuyingActivity extends Activity {
     public void onResume() {
         super.onResume();
 
+        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        setCurrentImage(currImage);
 
-        if (timer != null) {
-            timer.cancel();
-        }
-
-        timer = new Timer();
-        myTimerTask = new MyTimerTask();
-
-
-        timer.schedule(myTimerTask, 0, 1000);
-    }
-
-    class MyTimerTask extends TimerTask {
-
-        @Override
-        public void run() {
-
-            //Toast.makeText(WaitActivity.this, "Please wait for other players register", Toast.LENGTH_LONG).show();
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-
-                    setCurrentImage(PlayingActivity.currImage);
-
-                }
-            });
-        }
     }
 
     private void setCurrentImage(String image) {
 
         final ImageView imageView = (ImageView) findViewById(R.id.imageView2);
-        switch (currImage){
-            case "atlantic_avenue":
-                imageView.setImageResource(R.drawable.atlantic_avenue);
-                break;
-            case "baltic_avenue":
-                imageView.setImageResource(R.drawable.baltic_avenue);
-                break;
-            case "bo_railroad":
-                imageView.setImageResource(R.drawable.bo_railroad);
-                break;
-            case "board_walk":
-                imageView.setImageResource(R.drawable.board_walk);
-                break;
-            case "connecticut_avenue":
-                imageView.setImageResource(R.drawable.connecticut_avenue);
-                break;
-            case "electric_company":
-                imageView.setImageResource(R.drawable.electric_company);
-                break;
-            case "illinois_avenue":
-                imageView.setImageResource(R.drawable.illinois_avenue);
-                break;
-            case "indiana_avenue":
-                imageView.setImageResource(R.drawable.indiana_avenue);
-                break;
-            case "kentucky_avenue":
-                imageView.setImageResource(R.drawable.kentucky_avenue);
-                break;
-            case "marvin_gardens":
-                imageView.setImageResource(R.drawable.marvin_gardens);
-                break;
-            case "mediterranean_avenue":
+        switch (image){
+            case "1":
                 imageView.setImageResource(R.drawable.mediterranean_avenue);
                 break;
-            case "new_york_avenue":
-                imageView.setImageResource(R.drawable.new_york_avenue);
+            case "3":
+                imageView.setImageResource(R.drawable.baltic_avenue);
                 break;
-            case "north_carolina_avenue":
-                imageView.setImageResource(R.drawable.north_carolina_avenue);
-                break;
-            case "oriental_avenue":
-                imageView.setImageResource(R.drawable.oriental_avenue);
-                break;
-            case "pacific_avenue":
-                imageView.setImageResource(R.drawable.pacific_avenue);
-                break;
-            case "park_place":
-                imageView.setImageResource(R.drawable.park_place);
-                break;
-            case "pennsylvania_avenue":
-                imageView.setImageResource(R.drawable.pennsylvania_avenue);
-                break;
-            case "pennsylvania_railroad":
-                imageView.setImageResource(R.drawable.pennsylvania_railroad);
-                break;
-            case "reading_railroad":
+            case "5":
                 imageView.setImageResource(R.drawable.reading_railroad);
                 break;
-            case "short_line_railroad":
-                imageView.setImageResource(R.drawable.short_line_railroad);
+            case "6":
+                imageView.setImageResource(R.drawable.oriental_avenue);
                 break;
-            case "st_charles_place":
-                imageView.setImageResource(R.drawable.st_charles_place);
-                break;
-            case "st_james_place":
-                imageView.setImageResource(R.drawable.st_james_place);
-                break;
-            case "states_avenue":
-                imageView.setImageResource(R.drawable.states_avenue);
-                break;
-            case "tennesse_avenue":
-                imageView.setImageResource(R.drawable.tennesse_avenue);
-                break;
-            case "ventnor_avenue":
-                imageView.setImageResource(R.drawable.ventnor_avenue);
-                break;
-            case "vermont_avenue":
+            case "8":
                 imageView.setImageResource(R.drawable.vermont_avenue);
                 break;
-            case "virginia_avenue":
+            case "9":
+                imageView.setImageResource(R.drawable.connecticut_avenue);
+                break;
+            case "11":
+                imageView.setImageResource(R.drawable.st_charles_place);
+                break;
+            case "12":
+                imageView.setImageResource(R.drawable.electric_company);
+                break;
+            case "13":
+                imageView.setImageResource(R.drawable.states_avenue);
+                break;
+            case "14":
                 imageView.setImageResource(R.drawable.virginia_avenue);
                 break;
-            case "water_works":
+            case "15":
+                imageView.setImageResource(R.drawable.pennsylvania_railroad);
+                break;
+            case "16":
+                imageView.setImageResource(R.drawable.st_james_place);
+                break;
+            case "18":
+                imageView.setImageResource(R.drawable.tennesse_avenue);
+                break;
+            case "19":
+                imageView.setImageResource(R.drawable.new_york_avenue);
+                break;
+            case "21":
+                imageView.setImageResource(R.drawable.kentucky_avenue);
+                break;
+            case "23":
+                imageView.setImageResource(R.drawable.indiana_avenue);
+                break;
+            case "24":
+                imageView.setImageResource(R.drawable.illinois_avenue);
+                break;
+            case "25":
+                imageView.setImageResource(R.drawable.bo_railroad);
+                break;
+            case "26":
+                imageView.setImageResource(R.drawable.atlantic_avenue);
+                break;
+            case "27":
+                imageView.setImageResource(R.drawable.ventnor_avenue);
+                break;
+            case "28":
                 imageView.setImageResource(R.drawable.water_works);
+                break;
+            case "29":
+                imageView.setImageResource(R.drawable.marvin_gardens);
+                break;
+            case "31":
+                imageView.setImageResource(R.drawable.pacific_avenue);
+                break;
+            case "32":
+                imageView.setImageResource(R.drawable.north_carolina_avenue);
+                break;
+            case "34":
+                imageView.setImageResource(R.drawable.pennsylvania_avenue);
+                break;
+            case "35":
+                imageView.setImageResource(R.drawable.short_line_railroad);
+                break;
+            case "37":
+                imageView.setImageResource(R.drawable.park_place);
+                break;
+            case "39":
+                imageView.setImageResource(R.drawable.board_walk);
                 break;
         }
 
