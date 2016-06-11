dd1e405e858fcf7617332b2d49ea701c2ecf817a
agfac <andregfcorreia@gmail.com>
2016-06-11 17:38:10 +0100

    time playing changed

--- app/src/main/java/com/example/fabiola/monopoly/PlayingActivity.java
+++ app/src/main/java/com/example/fabiola/monopoly/PlayingActivity.java
@@ -56,7 +56,7 @@ public class PlayingActivity extends Activity {
         myTimerTask = new MyTimerTask();
 
 
-        timer.schedule(myTimerTask, 0, 200);
+        timer.schedule(myTimerTask, 0, 500);
     }
 
     class MyTimerTask extends TimerTask {
