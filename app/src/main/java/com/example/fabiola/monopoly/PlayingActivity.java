aa2dc0898c893ab8740baa64d7f894e4b6d89022
agfac <andregfcorreia@gmail.com>
2016-06-11 03:42:20 +0100

    Images loaded

--- app/src/main/java/com/example/fabiola/monopoly/PlayingActivity.java
+++ app/src/main/java/com/example/fabiola/monopoly/PlayingActivity.java
@@ -96,8 +96,14 @@ public class PlayingActivity extends Activity {
             case "1":
                 imageView.setImageResource(R.drawable.mediterranean_avenue);
                 break;
+            case "2":
+                imageView.setImageResource(R.drawable.chance2);
+                break;
             case "3":
                 imageView.setImageResource(R.drawable.baltic_avenue);
+                break;
+            case "4":
+                imageView.setImageResource(R.drawable.income_tax);
                 break;
             case "5":
                 imageView.setImageResource(R.drawable.reading_railroad);
@@ -105,11 +111,17 @@ public class PlayingActivity extends Activity {
             case "6":
                 imageView.setImageResource(R.drawable.oriental_avenue);
                 break;
+            case "7":
+                imageView.setImageResource(R.drawable.chance2);
+                break;
             case "8":
                 imageView.setImageResource(R.drawable.vermont_avenue);
                 break;
             case "9":
                 imageView.setImageResource(R.drawable.connecticut_avenue);
+                break;
+            case "10":
+                imageView.setImageResource(R.drawable.just_visiting2);
                 break;
             case "11":
                 imageView.setImageResource(R.drawable.st_charles_place);
@@ -129,14 +141,23 @@ public class PlayingActivity extends Activity {
             case "16":
                 imageView.setImageResource(R.drawable.st_james_place);
                 break;
+            case "17":
+                imageView.setImageResource(R.drawable.community_chest);
+                break;
             case "18":
                 imageView.setImageResource(R.drawable.tennesse_avenue);
                 break;
             case "19":
                 imageView.setImageResource(R.drawable.new_york_avenue);
                 break;
+            case "20":
+                imageView.setImageResource(R.drawable.free_parking);
+                break;
             case "21":
                 imageView.setImageResource(R.drawable.kentucky_avenue);
+                break;
+            case "22":
+                imageView.setImageResource(R.drawable.chance2);
                 break;
             case "23":
                 imageView.setImageResource(R.drawable.indiana_avenue);
@@ -159,11 +180,17 @@ public class PlayingActivity extends Activity {
             case "29":
                 imageView.setImageResource(R.drawable.marvin_gardens);
                 break;
+            case "30":
+                imageView.setImageResource(R.drawable.go_to_jail2);
+                break;
             case "31":
                 imageView.setImageResource(R.drawable.pacific_avenue);
                 break;
             case "32":
                 imageView.setImageResource(R.drawable.north_carolina_avenue);
+                break;
+            case "33":
+                imageView.setImageResource(R.drawable.community_chest);
                 break;
             case "34":
                 imageView.setImageResource(R.drawable.pennsylvania_avenue);
@@ -171,8 +198,14 @@ public class PlayingActivity extends Activity {
             case "35":
                 imageView.setImageResource(R.drawable.short_line_railroad);
                 break;
+            case "36":
+                imageView.setImageResource(R.drawable.chance2);
+                break;
             case "37":
                 imageView.setImageResource(R.drawable.park_place);
+                break;
+            case "38":
+                imageView.setImageResource(R.drawable.luxury_tax2);
                 break;
             case "39":
                 imageView.setImageResource(R.drawable.board_walk);
